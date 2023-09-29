b5ffa5f3ff6708b0d0ca800dc39c4f0d477a876c
ishcenko <a_ishcenko@i.ua>
2023-09-29 19:42:24 +0300

    робота з поштою

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const cteateError = require("./createError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseErr = require("./handleMongooseErr");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   cteateError,
   ctrlWrapper,
   handleMongooseErr,
+  sendEmail,
 };
